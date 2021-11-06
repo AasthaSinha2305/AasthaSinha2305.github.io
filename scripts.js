e24346c22521cc6e3cb32379aa299e66efa8fc67
AASTHA SINHA <asthasinha2305@gmail.com>
2021-11-07 03:07:14 +0530

    new version added

--- scripts.js
+++ scripts.js
@@ -1,19 +1,68 @@
-/*
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        window.location.hash = hash;
-      });
-    }
-  });
-});
 
-*/
+
+// auto typing starts here
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
+// auto typing ends here
+
+
+
+
+
 var preloader = document.getElementById('loader');
 function homePage(){
   preloader.style.display = 'none';
@@ -38,21 +87,5 @@ $('.skill').waypoint({
         this.destroy();
     }, offset: '50%'
 });
-/*
-//Fixed-top menu
-function fixedHeader() {
-    var ww = $(window).scrollTop();
-    if(ww > 0){
-        $('.menu').addClass('menu--active');
-        $('.mobile-menu').addClass('mobile-menu--active');
-    }else{
-        $('.menu').removeClass('menu--active');
-        $('.mobile-menu').removeClass('mobile-menu--active');
-    }
-}
-fixedHeader();
-$(window).on('scroll', function () {
-    fixedHeader();
-});
-*/
+
 
